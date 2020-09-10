7a9f2f1adb529f677e29efe74817f40e0588ed96
Heritier <you@example.com>
2020-09-10 16:35:01 +0200

    o

---
+++ ui/js/firebase.js
@@ -0,0 +1,19 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD8hImNox3auUHB5MqWaffbP3QOTZmxzPc",
+    authDomain: "mybrand-76a4f.firebaseapp.com",
+    databaseURL: "https://mybrand-76a4f.firebaseio.com",
+    projectId: "mybrand-76a4f",
+    storageBucket: "mybrand-76a4f.appspot.com",
+    messagingSenderId: "231455653710",
+    appId: "1:231455653710:web:a1edbd40fadee1452e281f",
+    measurementId: "G-1SHLBX8XKF"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db=firebase.firestore();
+  const auth=firebase.auth();
+//   
+// 
+
+// #########################################
+
